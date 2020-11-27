e6ee944375bd43a0acd12bfdb92005d7357bdb61
Paul-Elie Pipelin <paul.elie@free.fr>
2020-11-27 19:10:55 +0100

    Clean + add dynamic cam

--- gl_run.js
+++ gl_run.js
@@ -9,34 +9,23 @@ var message;
 var shaderProgram;
 var angleX = 0;
 var angleY = 0;
-var zoomZ = 15.5;
-
-var eye = { x: 0.0, y: 0.0, z: 0.0 };
-var center = { x: 0.0, y: 0.0, z: 0.0 };
-var up = { x: 0.0, y: 1.0, z: 0.0 }; // glenne setted to 0 0 1 while online 0 1 0
-var FOVY = 45.0;
-
-eye.x = zoomZ * Math.sin(angleY) * Math.cos(angleX);
-eye.y = zoomZ * Math.sin(angleX);
-eye.z = zoomZ * Math.cos(angleY) * Math.cos(angleX);
+var keyboard = [0,0,0];
 
 //Texture
 var textures;
 
 //Vertex Shader
 var VertexLocation;
-var u_veyeLocation;
-var u_vInvMPLocation;
 
 //Fragment Shader
-var u_numsLocation;
-var u_eyeLocation;
 var u_timeLocation;
 var u_itrLocation;
 var u_render_modeLocation;
 var u_textureLocation;
 var u_texsizeLocation;
 var u_texLocations = [];
+var u_mouseLocation;
+var u_keyboardLocation;
 
 //render shader
 var renderProgram;
@@ -68,7 +57,7 @@ function runGL() {
 	
 	//register
 	canvas.onmousedown = handleMouseDown;
-	canvas.oncontextmenu = function (ev) { return false; };
+	// canvas.oncontextmenu = function (ev) { return false; };
 	document.onmouseup = handleMouseUp;
 	document.onmousemove = handleMouseMove;
 	document.onkeydown = handleKeyDown;
@@ -128,29 +117,28 @@ function initializeShader() {
 
 	u_textureLocationc = gl.getUniformLocation(renderProgram, "texture");
 
-	//create path tracer shader
+	// Create path tracer shader
 	var vs = getShaderSource(document.getElementById("vs_pathTracer"));
 	var fs = getShaderSource(document.getElementById("fs_pathTracer"));
 
 	shaderProgram = createProgram(gl, vs, fs, message);
 
-	//Vertex Shader
+	// Vertex Shader
 	VertexLocation = gl.getAttribLocation(shaderProgram, "aVertex");
 	gl.enableVertexAttribArray(VertexLocation);
 
-	u_veyeLocation = gl.getUniformLocation(shaderProgram, "vcameraPos");
-	u_vInvMPLocation = gl.getUniformLocation(shaderProgram, "u_vInvMP");
-
-	//Fragment Shader        
+	// Fragment Shader        
 	u_timeLocation = gl.getUniformLocation(shaderProgram, "time");
 	u_itrLocation = gl.getUniformLocation(shaderProgram, "u_iterations");
 	u_render_modeLocation = gl.getUniformLocation(shaderProgram, "u_render_mode");
-	//Don't k why this line doesn't work
-	u_numsLocation = gl.getUniformLocation(shaderProgram, "objnums");
-	u_eyeLocation = gl.getUniformLocation(shaderProgram, "cameraPos");
 
 	u_textureLocation = gl.getUniformLocation(shaderProgram, "texture");
 	u_texsizeLocation = gl.getUniformLocation(shaderProgram, "texsize");
+
+	// Move
+	u_mouseLocation = gl.getUniformLocation(shaderProgram, "mouse");
+	u_keyboardLocation = gl.getUniformLocation(shaderProgram, "keyboard");
+
 }
 
 function animate() {
@@ -163,22 +151,7 @@ function animate() {
 		gl.useProgram(shaderProgram);
 
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-		var modelview = mat4.create();
-		mat4.lookAt([eye.x, eye.y, eye.z], [center.x, center.y, center.z], [up.x, up.y, up.z], modelview);
-
-		var projection = mat4.create();
-		mat4.perspective(FOVY, canvas.width / canvas.height, 0.1, 100.0, projection);
-
-		var modelviewprojection = mat4.create();
-		mat4.multiply(projection, modelview, modelviewprojection);
-
-		var inversemp = mat4.create();
-		mat4.inverse(modelviewprojection, inversemp);
-		
-		gl.uniformMatrix4fv(u_vInvMPLocation, false, inversemp);
-		gl.uniform3f(u_veyeLocation, eye.x, eye.y, eye.z);
-		gl.uniform3f(u_eyeLocation, eye.x, eye.y, eye.z);
+	
 		gl.uniform1f(u_timeLocation, time);
 		gl.uniform1f(u_itrLocation, iterations);
 		gl.uniform1f(u_render_modeLocation, render_mode);
@@ -189,6 +162,10 @@ function animate() {
 		gl.activeTexture(gl.TEXTURE0);
 		gl.bindTexture(gl.TEXTURE_2D, textures[0]);
 		gl.uniform1i(u_textureLocation, 0);
+
+		// Move 
+		gl.uniform2f(u_mouseLocation, angleX, angleY);
+		gl.uniform3f(u_keyboardLocation, keyboard[0], keyboard[1], keyboard[2]);
 
 		gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
 		gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffer);
@@ -251,6 +228,7 @@ function handleMouseDown(event) {
 		mouseLeftDown = false;
 		mouseRightDown = true;
 		mouseMidDown = false;
+		return;
 	}
 	else if (event.button == 0) {
 		mouseLeftDown = true;
@@ -276,6 +254,7 @@ function handleMouseMove(event) {
 	if (!(mouseLeftDown || mouseRightDown || mouseMidDown)) {
 		return;
 	}
+	if(mouseRightDown) return;
 	var newX = event.clientX;
 	var newY = event.clientY;
 
@@ -290,26 +269,12 @@ function handleMouseMove(event) {
 		// don't go upside down
 		angleX = Math.max(angleX, -Math.PI / 2 + 0.01);
 		angleX = Math.min(angleX, Math.PI / 2 - 0.01);
-
-		eye.x = zoomZ * Math.sin(angleY) * Math.cos(angleX);
-		eye.y = zoomZ * Math.sin(angleX);
-		eye.z = zoomZ * Math.cos(angleY) * Math.cos(angleX);
 	}
 	else if (mouseRightDown) {
-		zoomZ += 0.01 * deltaY;
-		zoomZ = Math.min(Math.max(zoomZ, 4.0), 20.0);
-
-		eye.x = zoomZ * Math.sin(angleY) * Math.cos(angleX);
-		eye.y = zoomZ * Math.sin(angleX);
-		eye.z = zoomZ * Math.cos(angleY) * Math.cos(angleX);
 	}
 	else if (mouseMidDown) {
-		center.x -= 0.01 * deltaX;
-		center.y += 0.01 * deltaY;
-		eye.x -= 0.01 * deltaX;
-		eye.y += 0.01 * deltaY;
 	}
-
+	// console.log(angleX, angleY);
 	lastMouseX = newX;
 	lastMouseY = newY;
 
@@ -317,8 +282,32 @@ function handleMouseMove(event) {
 }
 
 function handleKeyDown(event) {
-	if (event.keyCode == 32)
+	if (event.code == "Space")
 		pause = !pause;
+	if (event.code == "KeyA") {
+		++keyboard[1];
+		iterations = 0;
+	}
+	if (event.code == "KeyD") {
+		--keyboard[1];
+		iterations = 0;
+	}
+	if (event.code == "KeyW") {
+		++keyboard[0];
+		iterations = 0;
+	}
+	if (event.code == "KeyS") {
+		--keyboard[0];
+		iterations = 0;
+	}
+	if (event.code == "ShiftLeft" || event.code == "ShiftRight") {
+		--keyboard[2];
+		iterations = 0;
+	}
+	if (event.code == "ControlLeft" || event.code == "ControlRight") {
+		++keyboard[2];
+		iterations = 0;
+	}
 }
 
 function change_render_mode(i) {
