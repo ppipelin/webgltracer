636f7580901e9b2998092c7359fc3c1ef8d0a7d3
ppipelin <ppipelin@users.noreply.github.com>
2023-05-21 17:11:00 +0000

    deploy: d0633e3bfe9d839301ce855fba3921738049c07e

--- gl_run.js
+++ gl_run.js
@@ -72,6 +72,7 @@ function runGL() {
 	document.getElementById("time").innerHTML += "Initialize Shader: " + (end - begin).toString() + " ms<br/>";
 
 	initDefaultScene();
+	// initObjScene();
 
 	animate();
 
@@ -246,8 +247,8 @@ function AddObjsAttr(i) {
 	// bsdf_number: [0.0, 3.0] to[0, 255]
 	attributes[21 * i + 2] = 255.0 * Datas[i].obj_bsdf_number / 3.0;
 	// vertices:[-100.0,100.0] to [0,255] times 3
-	const mind = -100.0;
-	const maxd = 100.0;
+	const mind = -10.0;
+	const maxd = 10.0;
 	attributes[21 * i + 3] = 255.0 * (Datas[i].obj_v1[0] - mind) / (maxd - mind);
 	attributes[21 * i + 4] = 255.0 * (Datas[i].obj_v1[1] - mind) / (maxd - mind);
 	attributes[21 * i + 5] = 255.0 * (Datas[i].obj_v1[2] - mind) / (maxd - mind);
@@ -274,48 +275,6 @@ function AddObjsAttr(i) {
 	attributes[21 * i + 20] = 255.0;
 }
 
-function addSphere() {
-	if (Datas.length == 63)
-		return;
-	Datas.push({
-		obj_type: 0,
-		obj_textureType: 0,
-		obj_v1: [Math.random() * 100.0 - 50.0, Math.random() * 100.0 - 50.0, Math.random() * 100.0 - 50.0],
-		obj_v2: [0.0, 0.0, 0.0],
-		obj_v3: [0.0, 0.0, 0.0],
-		obj_bsdf_number: Math.random() * 3.0,
-		obj_albedo: [Math.random(), Math.random(), Math.random()],
-		obj_emissive: [Math.random(), Math.random(), Math.random()],
-		obj_eta: Math.random() * 10.0,
-		obj_shininess: Math.random(),
-	});
-
-	AddObjsAttr(Datas.length - 1);
-
-	u_iterations = 0;
-}
-
-function addTriangle() {
-	if (Datas.length == 63)
-		return;
-	Datas.push({
-		obj_type: 1,
-		obj_textureType: 0,
-		obj_v1: [Math.random() * 200.0 - 50.0, Math.random() * 200.0 - 50.0, Math.random() * 200.0 - 50.0],
-		obj_v2: [Math.random() * 200.0 - 50.0, Math.random() * 200.0 - 50.0, Math.random() * 200.0 - 50.0],
-		obj_v3: [Math.random() * 200.0 - 50.0, Math.random() * 200.0 - 50.0, Math.random() * 200.0 - 50.0],
-		obj_bsdf_number: Math.random() * 3.0,
-		obj_albedo: [Math.random(), Math.random(), Math.random()],
-		obj_emissive: [Math.random(), Math.random(), Math.random()],
-		obj_eta: Math.random() * 10.0,
-		obj_shininess: Math.random(),
-	});
-
-	AddObjsAttr(Datas.length - 1);
-
-	u_iterations = 0;
-}
-
 function rotationMatrix(axis, angle) {
 	const ratio = Math.sqrt(axis[0] ** 2 + axis[1] ** 2 + axis[2] ** 2);
 	axis[0] = axis[0] / ratio;
@@ -330,9 +289,6 @@ function rotationMatrix(axis, angle) {
 }
 
 function addCube(datas, position, scale = 1, axis = glMatrix.vec3.fromValues(0, 0, 1), angle = 0, obj_bsdf_number = 0, obj_albedo = glMatrix.vec3.fromValues(1, 1, 1), obj_emissive = glMatrix.vec3.fromValues(0, 0, 0), obj_eta = 1.0, obj_shininess = 0) {
-	if (datas.length == 63)
-		return;
-
 	shift = glMatrix.vec3.create();
 
 	// Left
@@ -358,9 +314,6 @@ function addCube(datas, position, scale = 1, axis = glMatrix.vec3.fromValues(0, 
 }
 
 function addQuad(datas, position, scale = glMatrix.vec2.fromValues(1, 1), axis = glMatrix.vec3.fromValues(0, 0, 1), angle = 0, obj_bsdf_number = 0, obj_albedo = glMatrix.vec3.fromValues(1, 1, 1), obj_emissive = glMatrix.vec3.fromValues(0, 0, 0), obj_eta = 1.0, obj_shininess = 0) {
-	if (datas.length == 63)
-		return;
-
 	R = rotationMatrix(axis, angle);
 
 	fl = glMatrix.vec3.create();
@@ -418,12 +371,13 @@ function initDefaultScene() {
 	// 	obj_eta: 0.0,
 	// 	obj_shininess: 0,
 	// });
+	DefaultDatas = [];
 
 	DefaultDatas.push({
 		obj_type: 0,
 		obj_textureType: 0,
 		obj_v1: [-2, 5, 5],
-		obj_v2: [(3 - 50) * 2, 0, 0],
+		obj_v2: [(3 - 5) * 2, 0, 0],
 		obj_v3: [0, 0, 0],
 		obj_bsdf_number: 0,
 		obj_albedo: [1, 1, 1],
@@ -436,7 +390,7 @@ function initDefaultScene() {
 		obj_type: 0,
 		obj_textureType: 0,
 		obj_v1: [-2, 0, 5],
-		obj_v2: [(3 - 50) * 2, 0, 0],
+		obj_v2: [(3 - 5) * 2, 0, 0],
 		obj_v3: [0, 0, 0],
 		obj_bsdf_number: 0,
 		obj_albedo: [1, 1, 1],
@@ -449,7 +403,7 @@ function initDefaultScene() {
 		obj_type: 0,
 		obj_textureType: 0,
 		obj_v1: [-2, -5, 5],
-		obj_v2: [(3 - 50) * 2, 0, 0],
+		obj_v2: [(3 - 5) * 2, 0, 0],
 		obj_v3: [0, 0, 0],
 		obj_bsdf_number: 0,
 		obj_albedo: [1, 1, 1],
@@ -476,7 +430,7 @@ function initDefaultScene() {
 		obj_type: 0,
 		obj_textureType: 0,
 		obj_v1: [5, 5, -5],
-		obj_v2: [(8 - 50) * 2, 0, 0],
+		obj_v2: [(8 - 5) * 2, 0, 0],
 		obj_v3: [0, 0, 0],
 		obj_bsdf_number: 1,
 		obj_albedo: [1, 1, 1],
@@ -484,6 +438,19 @@ function initDefaultScene() {
 		obj_eta: 1.0,
 		obj_shininess: 1,
 	});
+
+	// DefaultDatas.push({
+	// 	obj_type: 0,
+	// 	obj_textureType: 0,
+	// 	obj_v1: [0, 0, -2.5],
+	// 	obj_v2: [(4 - 5) * 2, 0, 0],
+	// 	obj_v3: [0, 0, 0],
+	// 	obj_bsdf_number: 3,
+	// 	obj_albedo: [1, 1, 1],
+	// 	obj_emissive: [0, 0, 0],
+	// 	obj_eta: 1.0,
+	// 	obj_shininess: 1,
+	// });
 
 	addCube(DefaultDatas,
 		glMatrix.vec3.fromValues(0, 0, -2.5),
@@ -510,7 +477,7 @@ function initDefaultScene() {
 		obj_type: 0,
 		obj_textureType: 0,
 		obj_v1: [0, 5, h],
-		obj_v2: [(s - 50) * 2, 0, 0],
+		obj_v2: [(s - 5) * 2, 0, 0],
 		obj_v3: [0, 0, 0],
 		obj_bsdf_number: 2,
 		obj_albedo: [1, 1, 1],
@@ -523,7 +490,7 @@ function initDefaultScene() {
 		obj_type: 0,
 		obj_textureType: 0,
 		obj_v1: [0, 2.5, h],
-		obj_v2: [(s - 50) * 2, 0, 0],
+		obj_v2: [(s - 5) * 2, 0, 0],
 		obj_v3: [0, 0, 0],
 		obj_bsdf_number: 2,
 		obj_albedo: [1, 1, 1],
@@ -536,7 +503,7 @@ function initDefaultScene() {
 		obj_type: 0,
 		obj_textureType: 0,
 		obj_v1: [0, 0, h],
-		obj_v2: [(s - 50) * 2, 0, 0],
+		obj_v2: [(s - 5) * 2, 0, 0],
 		obj_v3: [0, 0, 0],
 		obj_bsdf_number: 2,
 		obj_albedo: [1, 1, 1],
@@ -549,7 +516,7 @@ function initDefaultScene() {
 		obj_type: 0,
 		obj_textureType: 0,
 		obj_v1: [0, -2.5, h],
-		obj_v2: [(s - 50) * 2, 0, 0],
+		obj_v2: [(s - 5) * 2, 0, 0],
 		obj_v3: [0, 0, 0],
 		obj_bsdf_number: 2,
 		obj_albedo: [1, 1, 1],
@@ -562,7 +529,7 @@ function initDefaultScene() {
 		obj_type: 0,
 		obj_textureType: 0,
 		obj_v1: [0, -5, h],
-		obj_v2: [(s - 50) * 2, 0, 0],
+		obj_v2: [(s - 5) * 2, 0, 0],
 		obj_v3: [0, 0, 0],
 		obj_bsdf_number: 2,
 		obj_albedo: [1, 1, 1],
@@ -571,7 +538,106 @@ function initDefaultScene() {
 		obj_shininess: 1,
 	});
 
+	// Cornell
+	const size = 10;
+	const height = 5;
 
+	addQuad(
+		/* datas */ DefaultDatas,
+		/* position */ glMatrix.vec3.fromValues(0, 0, -height),
+		/* scale */ glMatrix.vec2.fromValues(size, size),
+		/* axis */ glMatrix.vec3.fromValues(0, 0, 1),
+		/* angle */ 0,
+		/* obj_bsdf_number */ 0,
+		/* obj_albedo */ glMatrix.vec3.fromValues(1, 1, 1),
+		/* obj_emissive */ glMatrix.vec3.fromValues(0, 0, 0),
+		/* obj_eta */ 1.0,
+		/* obj_shininess */ 0
+	);
+	addQuad(
+		/* datas */ DefaultDatas,
+		/* position */ glMatrix.vec3.fromValues(0, 0, height),
+		/* scale */ glMatrix.vec2.fromValues(size, size),
+		/* axis */ glMatrix.vec3.fromValues(0, 0, 1),
+		/* angle */ 0,
+		/* obj_bsdf_number */ 0,
+		/* obj_albedo */ glMatrix.vec3.fromValues(1, 1, 1),
+		/* obj_emissive */ glMatrix.vec3.fromValues(0, 0, 0),
+		/* obj_eta */ 1.0,
+		/* obj_shininess */ 0
+	);
+	addQuad(
+		/* datas */ DefaultDatas,
+		/* position */ glMatrix.vec3.fromValues(0, -size / 2, 0),
+		/* scale */ glMatrix.vec2.fromValues(size, size),
+		/* axis */ glMatrix.vec3.fromValues(1, 0, 0),
+		/* angle */ 90,
+		/* obj_bsdf_number */ 0,
+		/* obj_albedo */ glMatrix.vec3.fromValues(1, 0.25, 0.25),
+		/* obj_emissive */ glMatrix.vec3.fromValues(0, 0, 0),
+		/* obj_eta */ 1.0,
+		/* obj_shininess */ 0
+	);
+	addQuad(
+		/* datas */ DefaultDatas,
+		/* position */ glMatrix.vec3.fromValues(0, size / 2, 0),
+		/* scale */ glMatrix.vec2.fromValues(size, size),
+		/* axis */ glMatrix.vec3.fromValues(1, 0, 0),
+		/* angle */ 90,
+		/* obj_bsdf_number */ 0,
+		/* obj_albedo */ glMatrix.vec3.fromValues(0.25, 1, 0.25),
+		/* obj_emissive */ glMatrix.vec3.fromValues(0, 0, 0),
+		/* obj_eta */ 1.0,
+		/* obj_shininess */ 0
+	);
+	addQuad(
+		/* datas */ DefaultDatas,
+		/* position */ glMatrix.vec3.fromValues(size / 2, 0, 0),
+		/* scale */ glMatrix.vec2.fromValues(size, size),
+		/* axis */ glMatrix.vec3.fromValues(0, 1, 0),
+		/* angle */ 90,
+		/* obj_bsdf_number */ 0,
+		/* obj_albedo */ glMatrix.vec3.fromValues(1, 1, 1),
+		/* obj_emissive */ glMatrix.vec3.fromValues(0, 0, 0),
+		/* obj_eta */ 1.0,
+		/* obj_shininess */ 0
+	);
+
+	defaultScene();
+}
+
+function initObjScene() {
+	DefaultDatas = [];
+
+	DefaultDatas.push({
+		obj_type: 0,
+		obj_textureType: 0,
+		obj_v1: [-2, -5, 5],
+		obj_v2: [(3 - 5) * 2, 0, 0],
+		obj_v3: [0, 0, 0],
+		obj_bsdf_number: 0,
+		obj_albedo: [1, 1, 1],
+		obj_emissive: [1.0, 0.6, 0.6],
+		obj_eta: 1.0,
+		obj_shininess: 0,
+	});
+
+	let s = 3;
+	for (let i = 0; i < faces.length; ++i) {
+		// console.log(i, vertices[faces[i][0]], vertices[faces[i][1]], vertices[faces[i][2]]);
+		DefaultDatas.push({
+			obj_type: 1,
+			obj_textureType: 0,
+			obj_v1: [vertices[faces[i][0] - 1][0] * s, vertices[faces[i][0] - 1][1] * s, vertices[faces[i][0] - 1][2] * s],
+			obj_v2: [vertices[faces[i][1] - 1][0] * s, vertices[faces[i][1] - 1][1] * s, vertices[faces[i][1] - 1][2] * s],
+			obj_v3: [vertices[faces[i][2] - 1][0] * s, vertices[faces[i][2] - 1][1] * s, vertices[faces[i][2] - 1][2] * s],
+			obj_bsdf_number: 0,
+			obj_albedo: [1, 1, 1],
+			obj_emissive: [0, 0, 0],
+			obj_eta: 1.0,
+			obj_shininess: 0,
+		});
+	}
 
 	// Cornell
 	const size = 10;
@@ -642,7 +708,7 @@ function initDefaultScene() {
 }
 
 function defaultScene() {
-	Datas.length = 0;
+	Datas = [];
 
 	for (let i = 0; i < DefaultDatas.length; i++) {
 		Datas[i] = DefaultDatas[i];
@@ -794,3 +860,268 @@ function change_scene(i) {
 	u_scene = i;
 	console.log("Changed scene to: " + u_scene);
 }
+
+const vertices_old = [
+	[1.250000, 0.000000, 0.000000],
+	[1.176777, 0.176777, 0.000000],
+	[1.000000, 0.250000, 0.000000],
+	[0.823223, 0.176777, 0.000000],
+	[0.750000, 0.000000, 0.000000],
+	[0.823223, - 0.176777, 0.000000],
+	[1.000000, - 0.250000, 0.000000],
+	[1.176777, - 0.176777, 0.000000],
+	[0.883883, 0.000000, - 0.883884],
+	[0.832107, 0.176777, - 0.832107],
+	[0.707107, 0.250000, - 0.707107],
+	[0.582107, 0.176777, - 0.582107],
+	[0.530330, 0.000000, - 0.530330],
+	[0.582107, - 0.176777, - 0.582107],
+	[0.707107, - 0.250000, - 0.707107],
+	[0.832107, - 0.176777, - 0.832107],
+	[0.000000, 0.000000, - 1.250000],
+	[0.000000, 0.176777, - 1.176777],
+	[0.000000, 0.250000, - 1.000000],
+	[0.000000, 0.176777, - 0.823223],
+	[0.000000, 0.000000, - 0.750000],
+	[0.000000, - 0.176777, - 0.823223],
+	[0.000000, - 0.250000, - 1.000000],
+	[0.000000, - 0.176777, - 1.176777],
+	[- 0.883884, 0.000000, - 0.883883],
+	[- 0.832107, 0.176777, - 0.832107],
+	[- 0.707107, 0.250000, - 0.707107],
+	[- 0.582107, 0.176777, - 0.582107],
+	[- 0.530330, 0.000000, - 0.530330],
+	[- 0.582107, - 0.176777, - 0.582107],
+	[- 0.707107, - 0.250000, - 0.707107],
+	[- 0.832107, - 0.176777, - 0.832107],
+	[- 1.250000, 0.000000, - 0.000000],
+	[- 1.176777, 0.176777, - 0.000000],
+	[- 1.000000, 0.250000, - 0.000000],
+	[- 0.823223, 0.176777, - 0.000000],
+	[- 0.750000, 0.000000, - 0.000000],
+	[- 0.823223, - 0.176777, - 0.000000],
+	[- 1.000000, - 0.250000, - 0.000000],
+	[- 1.176777, - 0.176777, - 0.000000],
+	[- 0.883884, 0.000000, 0.883883],
+	[- 0.832107, 0.176777, 0.832107],
+	[- 0.707107, 0.250000, 0.707107],
+	[- 0.582107, 0.176777, 0.582107],
+	[- 0.530330, 0.000000, 0.530330],
+	[- 0.582107, - 0.176777, 0.582107],
+	[- 0.707107, - 0.250000, 0.707107],
+	[- 0.832107, - 0.176777, 0.832107],
+	[0.000000, 0.000000, 1.250000],
+	[0.000000, 0.176777, 1.176777],
+	[0.000000, 0.250000, 1.000000],
+	[0.000000, 0.176777, 0.823223],
+	[0.000000, 0.000000, 0.750000],
+	[0.000000, - 0.176777, 0.823223],
+	[0.000000, - 0.250000, 1.000000],
+	[0.000000, - 0.176777, 1.176777],
+	[0.883883, 0.000000, 0.883884],
+	[0.832106, 0.176777, 0.832107],
+	[0.707106, 0.250000, 0.707107],
+	[0.582106, 0.176777, 0.582107],
+	[0.530330, 0.000000, 0.530330],
+	[0.582106, - 0.176777, 0.582107],
+	[0.707106, - 0.250000, 0.707107],
+	[0.832106, - 0.176777, 0.832107],
+];
+
+const vertices = [
+	[0.765465, - 0.883883, - 0.441942],
+	[0.828879, - 0.707107, - 0.478553],
+	[0.765465, - 0.530330, - 0.441942],
+	[0.612372, - 0.457107, - 0.353553],
+	[0.459279, - 0.530330, - 0.265165],
+	[0.395866, - 0.707107, - 0.228553],
+	[0.459279, - 0.883883, - 0.265165],
+	[0.612372, - 0.957107, - 0.353553],
+	[0.099324, - 0.625000, - 1.077965],
+	[0.201759, - 0.463388, - 1.077320],
+	[0.232552, - 0.323223, - 0.950761],
+	[0.173666, - 0.286612, - 0.772425],
+	[0.059594, - 0.375000, - 0.646779],
+	[- 0.042841, - 0.536612, - 0.647425],
+	[- 0.073634, - 0.676777, - 0.773984],
+	[- 0.014747, - 0.713388, - 0.952320],
+	[- 0.625000, - 0.000000, - 1.082532],
+	[- 0.480135, 0.125000, - 1.081618],
+	[- 0.346907, 0.176777, - 0.954414],
+	[- 0.303358, 0.125000, - 0.775432],
+	[- 0.375000, - 0.000000, - 0.649519],
+	[- 0.519865, - 0.125000, - 0.650432],
+	[- 0.653093, - 0.176777, - 0.777637],
+	[- 0.696641, - 0.125000, - 0.956618],
+	[- 0.983208, 0.625000, - 0.452965],
+	[- 0.817359, 0.713388, - 0.488931],
+	[- 0.633473, 0.676777, - 0.450761],
+	[- 0.539266, 0.536612, - 0.360813],
+	[- 0.589925, 0.375000, - 0.271779],
+	[- 0.755773, 0.286612, - 0.235813],
+	[- 0.939659, 0.323223, - 0.273984],
+	[- 1.033866, 0.463388, - 0.363931],
+	[- 0.765466, 0.883883, 0.441942],
+	[- 0.612372, 0.957107, 0.353553],
+	[- 0.459279, 0.883883, 0.265165],
+	[- 0.395866, 0.707107, 0.228553],
+	[- 0.459279, 0.530330, 0.265165],
+	[- 0.612372, 0.457107, 0.353553],
+	[- 0.765466, 0.530330, 0.441942],
+	[- 0.828879, 0.707107, 0.478553],
+	[- 0.099324, 0.625000, 1.077965],
+	[0.014747, 0.713388, 0.952320],
+	[0.073634, 0.676777, 0.773984],
+	[0.042840, 0.536612, 0.647425],
+	[- 0.059595, 0.375000, 0.646779],
+	[- 0.173666, 0.286612, 0.772425],
+	[- 0.232553, 0.323223, 0.950761],
+	[- 0.201759, 0.463388, 1.077320],
+	[0.625000, - 0.000000, 1.082532],
+	[0.696642, 0.125000, 0.956618],
+	[0.653093, 0.176777, 0.777637],
+	[0.519865, 0.125000, 0.650432],
+	[0.375000, - 0.000000, 0.649519],
+	[0.303358, - 0.125000, 0.775432],
+	[0.346907, - 0.176777, 0.954414],
+	[0.480135, - 0.125000, 1.081618],
+	[0.983208, - 0.625000, 0.452966],
+	[1.033866, - 0.463388, 0.363932],
+	[0.939659, - 0.323223, 0.273985],
+	[0.755773, - 0.286611, 0.235814],
+	[0.589925, - 0.375000, 0.271780],
+	[0.539266, - 0.536611, 0.360814],
+	[0.633473, - 0.676776, 0.450761],
+	[0.817359, - 0.713388, 0.488932],
+];
+
+const faces = [
+	[9, 2, 1],
+	[2, 11, 3],
+	[11, 4, 3],
+	[12, 5, 4],
+	[13, 6, 5],
+	[6, 15, 7],
+	[15, 8, 7],
+	[16, 1, 8],
+	[9, 18, 10],
+	[18, 11, 10],
+	[19, 12, 11],
+	[20, 13, 12],
+	[21, 14, 13],
+	[22, 15, 14],
+	[23, 16, 15],
+	[24, 9, 16],
+	[25, 18, 17],
+	[26, 19, 18],
+	[19, 28, 20],
+	[20, 29, 21],
+	[29, 22, 21],
+	[30, 23, 22],
+	[31, 24, 23],
+	[32, 17, 24],
+	[33, 26, 25],
+	[34, 27, 26],
+	[35, 28, 27],
+	[36, 29, 28],
+	[37, 30, 29],
+	[30, 39, 31],
+	[39, 32, 31],
+	[40, 25, 32],
+	[33, 42, 34],
+	[42, 35, 34],
+	[43, 36, 35],
+	[44, 37, 36],
+	[37, 46, 38],
+	[46, 39, 38],
+	[47, 40, 39],
+	[48, 33, 40],
+	[49, 42, 41],
+	[42, 51, 43],
+	[43, 52, 44],
+	[52, 45, 44],
+	[45, 54, 46],
+	[54, 47, 46],
+	[55, 48, 47],
+	[56, 41, 48],
+	[57, 50, 49],
+	[58, 51, 50],
+	[59, 52, 51],
+	[60, 53, 52],
+	[53, 62, 54],
+	[62, 55, 54],
+	[55, 64, 56],
+	[64, 49, 56],
+	[1, 58, 57],
+	[2, 59, 58],
+	[3, 60, 59],
+	[4, 61, 60],
+	[5, 62, 61],
+	[6, 63, 62],
+	[7, 64, 63],
+	[8, 57, 64],
+	[9, 10, 2],
+	[2, 10, 11],
+	[11, 12, 4],
+	[12, 13, 5],
+	[13, 14, 6],
+	[6, 14, 15],
+	[15, 16, 8],
+	[16, 9, 1],
+	[9, 17, 18],
+	[18, 19, 11],
+	[19, 20, 12],
+	[20, 21, 13],
+	[21, 22, 14],
+	[22, 23, 15],
+	[23, 24, 16],
+	[24, 17, 9],
+	[25, 26, 18],
+	[26, 27, 19],
+	[19, 27, 28],
+	[20, 28, 29],
+	[29, 30, 22],
+	[30, 31, 23],
+	[31, 32, 24],
+	[32, 25, 17],
+	[33, 34, 26],
+	[34, 35, 27],
+	[35, 36, 28],
+	[36, 37, 29],
+	[37, 38, 30],
+	[30, 38, 39],
+	[39, 40, 32],
+	[40, 33, 25],
+	[33, 41, 42],
+	[42, 43, 35],
+	[43, 44, 36],
+	[44, 45, 37],
+	[37, 45, 46],
+	[46, 47, 39],
+	[47, 48, 40],
+	[48, 41, 33],
+	[49, 50, 42],
+	[42, 50, 51],
+	[43, 51, 52],
+	[52, 53, 45],
+	[45, 53, 54],
+	[54, 55, 47],
+	[55, 56, 48],
+	[56, 49, 41],
+	[57, 58, 50],
+	[58, 59, 51],
+	[59, 60, 52],
+	[60, 61, 53],
+	[53, 61, 62],
+	[62, 63, 55],
+	[55, 63, 64],
+	[64, 57, 49],
+	[1, 2, 58],
+	[2, 3, 59],
+	[3, 4, 60],
+	[4, 5, 61],
+	[5, 6, 62],
+	[6, 7, 63],
+	[7, 8, 64],
+	[8, 1, 57],
+];
